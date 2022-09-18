6c4bfe20a151192efa803fdb11b52d71102c5f6c
HN6039940 <hn6039940@gmail.com>
2022-09-18 10:53:53 +0900

    add comment and make enter your api key in config.js

--- src/js/config.js
+++ src/js/config.js
@@ -2,4 +2,5 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const SET_TIME = 2.5;
 export const RES_PER_PAGE = 10;
-export const KEY = '835e7a3f-2de5-4dc5-b342-f38b5606b9d2';
+// USE Your API KEY
+export const KEY = 'YOUR API KEY';
