9efcbe4cbd124b4aa97564b905d5f7a022a2e00d
HN6039940 <hn6039940@gmail.com>
2022-09-19 00:47:12 +0900

    change config.js

--- src/js/config.js
+++ src/js/config.js
@@ -3,4 +3,4 @@ export const TIMEOUT_SEC = 10;
 export const SET_TIME = 2.5;
 export const RES_PER_PAGE = 10;
 // USE Your API KEY
-export const KEY = 'YOUR API KEY';
+export const KEY = '835e7a3f-2de5-4dc5-b342-f38b5606b9d2';
